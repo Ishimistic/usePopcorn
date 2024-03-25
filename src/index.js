437335fc86ad7bda3d56ed202bed9de2d3a53086
IshticBlue <156532026+Ishimistic@users.noreply.github.com>
2024-03-25 19:55:40 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+// import StarRating from './StarRating';
+
+
+//IMPORTANT
+/*
+function Test(){
+  const[movieRating,setMovieRating] = useState(0);
+  return (
+    <div>
+      <StarRating maxratings={5} color='salmon' onSetRating={setMovieRating}/>
+      <p>This movie has {movieRating} rating</p>
+    </div>
+  )
+}
+*/
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+    {/* // <StarRating maxratings={10}/> */}
+    {/* // <StarRating maxratings={5} messages={["Terrible","Bad", "Okay", "Good", "Amazing" ]}/>
+    <StarRating maxratings={5} color="red" className="test" defaultRating={2}/> */}
+    {/* <StarRating /> */}
+    {/* <Test /> */}
+  </React.StrictMode>
+);
+
