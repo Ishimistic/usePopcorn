5b595021201394901204522107c6ee4815f27f8c
IshticBlue <156532026+Ishimistic@users.noreply.github.com>
2024-10-06 19:57:18 +0530

    Update useKey.js

--- src/useKey.js
+++ src/useKey.js
@@ -1,18 +1,23 @@
-import { useEffect } from "react";
 
-export function useKey(key, action){
-    useEffect(
-        function () {
-          function callback(e) {
-            if (e.code.toLowerCase() === key.toLowerCase()) {
-              action();
-            }
-          }
-          document.addEventListener("keydown", callback);
-          return function () {
-            document.removeEventListener("keydown", callback);
-          };
-        },
-        [key, action]
-      );
+import { useEffect, useCallback } from "react";
+
+export function useKey(key, action) {
+  
+  const callback = useCallback(
+    (e) => {
+      if (e.code.toLowerCase() === key.toLowerCase()) {
+        action();
+      }
+    },
+    [key, action]
+  );
+
+  useEffect(() => {
+  document.addEventListener("keydown", callback);
+
+    // Cleanup function to remove event listener
+    return () => {
+      document.removeEventListener("keydown", callback);
+    };
+  }, [callback]);
 }
